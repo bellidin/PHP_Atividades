00e4d85fd1934216bcffe33f05ded217982a064d
bellidin <nardineduc@gmail.com>
2024-11-18 14:25:11 -0300

    Resolução atividade 04

--- 4_condicionais.php
+++ 4_condicionais.php
@@ -1,6 +1,22 @@
 <?php
 
-// Digitar PHP (1º Aqui)
+// Verificar os dados do formulário utilizando o método POST
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $senha = $_POST['senha'];
+
+    if($senha === '210312'){
+        // redireciona para a página de 'boas-vindas'
+        header("Location:4b_bem_vindo.php");
+        exit();
+    }
+
+}else{
+
+    // mensagem de erro
+    $erro = "Senha incorreta. Tente novamente!";
+}
 
 ?>
 <!DOCTYPE html>
@@ -19,8 +35,11 @@
 
     <?php
 
-    // Digitar PHP (2º Aqui)
-
+    // 2° parte em PHP, retorna o valor de acordo com a condicional
+    // Exibe a mensagem de erro, se existir
+    if (isset($erro)) {
+        echo "<p style='color: red;'>$erro</p>";
+    }
     ?>
 </body>
 </html>
