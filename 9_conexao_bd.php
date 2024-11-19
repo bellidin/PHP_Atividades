7d9c2eb07b8282dbb8548babb1c807b9f7c6466a
bellidin <nardineduc@gmail.com>
2024-11-19 09:43:19 -0300

    Resolução atividade 09

--- 9_conexao_bd.php
+++ 9_conexao_bd.php
@@ -13,8 +13,28 @@
 
 <?php
 
-// Digitar PHP
+// ALTERAR de acordo com o número da porta
+// Verificar conexão com o XAMPP
+$servername = "localhost:3309";
+$username = "root";
+$password = "";
+$dbname = "exercicio";
 
+try {
+    // Tenta criar uma conexão com o banco de dados 
+    // Confere se as informações estão corretas
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verifica se houve algum erro na conexão 
+    if ($conn->connect_error) {
+        throw new Exception("Falha na conexão: " . $conn->connect_error);
+    }
+
+    echo "Conexão realizada com sucesso!";
+} catch (Exception $e) {
+    // Exibe uma mensagem de erro amigável
+    echo "Erro ao conectar ao banco de dados: " . $e->getmessage();
+}
 ?>
 
 
